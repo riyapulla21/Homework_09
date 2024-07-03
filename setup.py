2a7f391f0f400eb161abc8e206cfa4cd7d16169c
riyapulla21 <riyakpulla@gmail.com>
2024-07-03 17:37:34 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HW09',
+    version='1.0.0',
+    url='https://github.com/riyapulla21/Homework_09/tree/main/booklover',
+    author='riya pulla',
+    author_email='wbd3kw@virginia.edu',
+    description='exercise 9 practice package',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
